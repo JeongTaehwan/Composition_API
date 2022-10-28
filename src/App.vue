9a1996ba8f6a1fc6e590a17aeef135a8d64c84f1
JeongTaehwan <wjdxoghks11222@naver.com>
2022-10-28 14:47:18 +0900

    Update : HelloWorld.vue 제거 및 App.vue 수정

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <refReactive />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import refReactive from "./components/refReactive.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    refReactive,
+  },
+};
 </script>
 
 <style>
